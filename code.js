85c4e744a489c5ad2e56bf372c5be47bb5faf4e8
armandojsalazar <armando.salazar89@gmail.com>
2020-08-17 11:11:42 -0500

    Testing Intro Excersice 1 complete

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-// helloWorld function 
+// helloWorld function
+// function expression or anonymous function assignment
+// var helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+// function statement or named function
 function helloWorld() {
     return "Hello, World!";
+}
+function sayHello(){
+    return ''
 }
